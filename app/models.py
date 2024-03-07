36543d1ccb61ed416d6dee7817548a8b12f6f03b
JZG <jzg924@gmail.com>
2024-03-06 19:14:54 -0500

    initial commit

--- app/models.py
+++ app/models.py
@@ -1,4 +1,5 @@
 from . import db
+from werkzeug.security import generate_password_hash
 
 
 class UserProfile(db.Model):
@@ -6,12 +7,21 @@ class UserProfile(db.Model):
     # the class name. In this case a class name of UserProfile would create a
     # user_profile (singular) table, but if we specify __tablename__ we can change it
     # to `user_profiles` (plural) or some other name.
+    def  __init__(self,id,firstname,lastname,username,password):
+        self.id=id
+        self.first_name=firstname
+        self.last_name=lastname
+        self.username=username
+        self.password=password
+
     __tablename__ = 'user_profiles'
 
     id = db.Column(db.Integer, primary_key=True)
     first_name = db.Column(db.String(80))
     last_name = db.Column(db.String(80))
     username = db.Column(db.String(80), unique=True)
+    password = db.Column(db.String(128))
+    created_at = db.Column(db.DateTime, default=db.func.current_timestamp())
 
     def is_authenticated(self):
         return True
